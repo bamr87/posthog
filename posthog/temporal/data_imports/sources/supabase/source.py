01f9981f4ed4c0b7f2aaeec0c79b47a7e4c676d0
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-11-01 15:15:47 -0500

    change how this "inherits"

--- posthog/temporal/data_imports/sources/supabase/source.py
+++ posthog/temporal/data_imports/sources/supabase/source.py
@@ -5,19 +5,18 @@ from posthog.schema import (
 
 from posthog.temporal.data_imports.pipelines.pipeline.typings import SourceInputs, SourceResponse
 from posthog.temporal.data_imports.sources.common.base import BaseSource
+from posthog.temporal.data_imports.sources.common.mixins import SSHTunnelMixin, ValidateDatabaseHostMixin
 from posthog.temporal.data_imports.sources.common.registry import SourceRegistry
 from posthog.temporal.data_imports.sources.common.schema import SourceSchema
 from posthog.temporal.data_imports.sources.generated_configs import SupabaseSourceConfig
 from posthog.temporal.data_imports.sources.postgres.source import PostgresSource
 from posthog.warehouse.types import ExternalDataSourceType
 
+postgres = PostgresSource()
+
 
 @SourceRegistry.register
-class SupabaseSource(BaseSource[SupabaseSourceConfig]):
-    def __init__(self):
-        super().__init__()
-        self.pg = PostgresSource()
-
+class SupabaseSource(BaseSource[SupabaseSourceConfig], SSHTunnelMixin, ValidateDatabaseHostMixin):
     @property
     def source_type(self) -> ExternalDataSourceType:
         return ExternalDataSourceType.SUPABASE
@@ -29,16 +28,16 @@ class SupabaseSource(BaseSource[SupabaseSourceConfig]):
             iconPath="/static/services/supabase.png",
             caption="Enter your Supabase credentials to automatically pull your data into the PostHog Data warehouse",
             docsUrl="https://posthog.com/tutorials/supabase-query",
-            fields=self.pg.get_source_config.fields,
+            fields=postgres.get_source_config.fields,
             betaSource=True,
             featureFlag="supabase-dwh",
         )
 
     def validate_credentials(self, config: SupabaseSourceConfig, team_id: int) -> tuple[bool, str | None]:
-        return self.pg.validate_credentials(config, team_id)
+        return postgres.validate_credentials(config, team_id)
 
     def get_schemas(self, config: SupabaseSourceConfig, team_id: int, with_counts: bool = False) -> list[SourceSchema]:
-        return self.pg.get_schemas(config, team_id, with_counts)
+        return postgres.get_schemas(config, team_id, with_counts)
 
     def source_for_pipeline(self, config: SupabaseSourceConfig, inputs: SourceInputs) -> SourceResponse:
-        return self.pg.source_for_pipeline(config, inputs)
+        return postgres.source_for_pipeline(config, inputs)
